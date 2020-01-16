f94beac62791b44aa57f5256bc0d9df64c3ddb99
johangutihaz <38494614+johangutihaz@users.noreply.github.com>
2020-01-16 20:52:41 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CarmodelList from './CarmodelList';
 function App() {
   return (
     <div className="App">
-      <img src='header.jpg' alt="Simply Easy Learning" width="200"
+      <img src='/header.jpg' alt="Simply Easy Learning" width="200"
          height="80"></img>
     < CarmodelList/>
     </div>
